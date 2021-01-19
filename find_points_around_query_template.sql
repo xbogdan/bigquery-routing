d7a7222330b468fab86100554e57a0470ff502aa
Bogdan BOAMFA <bboamfa.externe@solocal.com>
2021-01-19 20:35:44 +0200

    Set weight function.

--- find_points_around_query_template.sql
+++ find_points_around_query_template.sql
@@ -64,8 +64,57 @@ OPTIONS (
   library=["$BUCKET_FILE_PATH"]
 )
 AS """
+  const highwaySpeeds = {
+    motorway: 110,
+    trunk: 90,
+    primary: 80,
+    secondary: 70,
+    tertiary: 50,
+    unclassified: 50,
+    road: 50,
+    residential: 30,
+    service: 30,
+    living_street: 20
+  };
+
+  const unknowns = {};
+
+  function weightFn(a, b, props) {
+      let d = distance(point(a), point(b)) * 1000,
+          factor = 0.9,
+          type = props.highway,
+          forwardSpeed,
+          backwardSpeed;
+
+      if (props.maxspeed) {
+          forwardSpeed = backwardSpeed = Number(props.maxspeed);
+      } else {
+          let linkIndex = type.indexOf('_link');
+
+          if (linkIndex >= 0) {
+              type = type.substring(0, linkIndex);
+              factor *= 0.7;
+          }
+      
+          forwardSpeed = backwardSpeed = highwaySpeeds[type] * factor;
+      
+          if (!forwardSpeed) {
+              unknowns[type] = true;
+          }
+      }
+      
+      if (props.oneway && props.oneway !== 'no' || props.junction && props.junction === 'roundabout') {
+          backwardSpeed = null;
+      }
+
+      return {
+          forward: forwardSpeed && (d / (forwardSpeed / 3.6)),
+          backward: backwardSpeed && (d / (backwardSpeed / 3.6)),
+      };
+  }
+
   const start = {type: "Feature", geometry: { coordinates: [startx, starty], type: "Point" }};
-  const pathFinder = new geojsonPathFinder(JSON.parse(geojson));
+  const pathFinder = new geojsonPathFinder(JSON.parse(geojson), { weightFn: weightFn });
 
   const hull = pathFinder.getIsoDistanceConcaveHull(start, max_cost);
   
