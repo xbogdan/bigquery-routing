d1fb357c9b802ee792732047a2320b611ee26108
Bogdan BOAMFA <bboamfa.externe@solocal.com>
2021-01-19 17:50:44 +0200

    Deactivate unecessary func.

--- find_points_around_query_template.sql
+++ find_points_around_query_template.sql
@@ -39,23 +39,23 @@ AS """
 """;
 
 -- get the convex hull
-CREATE OR REPLACE FUNCTION `$PROJECT_ID.$DATASET.get_isodistance_convex_hull_from_geojson`(geojson STRING, startx FLOAT64, starty FLOAT64, max_cost FLOAT64) 
-RETURNS STRING LANGUAGE js
-OPTIONS (
-  library=["$BUCKET_FILE_PATH"]
-)
-AS """
-  const start = {type: "Feature", geometry: { coordinates: [startx, starty], type: "Point" }};
-  const pathFinder = new geojsonPathFinder(JSON.parse(geojson));
+-- CREATE OR REPLACE FUNCTION `$PROJECT_ID.$DATASET.get_isodistance_convex_hull_from_geojson`(geojson STRING, startx FLOAT64, starty FLOAT64, max_cost FLOAT64) 
+-- RETURNS STRING LANGUAGE js
+-- OPTIONS (
+--   library=["$BUCKET_FILE_PATH"]
+-- )
+-- AS """
+--   const start = {type: "Feature", geometry: { coordinates: [startx, starty], type: "Point" }};
+--   const pathFinder = new geojsonPathFinder(JSON.parse(geojson));
 
-  const hull = pathFinder.getIsoDistanceConvexHull(start, max_cost);
+--   const hull = pathFinder.getIsoDistanceConvexHull(start, max_cost);
   
-  try {
-    return hull;
-  } catch (e) {
-    return(null);
-  }
-""";
+--   try {
+--     return JSON.stringify(hull.geometry);
+--   } catch (e) {
+--     return(null);
+--   }
+-- """;
 
 -- get isochrone the concave hull
 CREATE OR REPLACE FUNCTION `$PROJECT_ID.$DATASET.get_isochrone_concave_hull_from_geojson`(geojson STRING, startx FLOAT64, starty FLOAT64, max_cost FLOAT64) 
@@ -70,30 +70,30 @@ AS """
   const hull = pathFinder.getIsoDistanceConcaveHull(start, max_cost);
   
   try {
-    return hull;
+    return JSON.stringify(hull.geometry);
   } catch (e) {
     return(null);
   }
 """;
 
 -- get isochrone the convex hull
-CREATE OR REPLACE FUNCTION `$PROJECT_ID.$DATASET.get_isochrone_convex_hull_from_geojson`(geojson STRING, startx FLOAT64, starty FLOAT64, max_cost FLOAT64) 
-RETURNS STRING LANGUAGE js
-OPTIONS (
-  library=["$BUCKET_FILE_PATH"]
-)
-AS """
-  const start = {type: "Feature", geometry: { coordinates: [startx, starty], type: "Point" }};
-  const pathFinder = new geojsonPathFinder(JSON.parse(geojson));
+-- CREATE OR REPLACE FUNCTION `$PROJECT_ID.$DATASET.get_isochrone_convex_hull_from_geojson`(geojson STRING, startx FLOAT64, starty FLOAT64, max_cost FLOAT64) 
+-- RETURNS STRING LANGUAGE js
+-- OPTIONS (
+--   library=["$BUCKET_FILE_PATH"]
+-- )
+-- AS """
+--   const start = {type: "Feature", geometry: { coordinates: [startx, starty], type: "Point" }};
+--   const pathFinder = new geojsonPathFinder(JSON.parse(geojson));
 
-  const hull = pathFinder.getIsoDistanceConvexHull(start, max_cost);
+--   const hull = pathFinder.getIsoDistanceConvexHull(start, max_cost);
   
-  try {
-    return hull;
-  } catch (e) {
-    return(null);
-  }
-""";
+--   try {
+--     return JSON.stringify(hull.geometry);
+--   } catch (e) {
+--     return(null);
+--   }
+-- """;
 
 -- helper to find the nearest point if the input point is not in the dataset
 CREATE OR REPLACE FUNCTION `$PROJECT_ID.$DATASET.find_nearest_point`(mypoint GEOGRAPHY, mypoints array<GEOGRAPHY>) AS ((
@@ -138,22 +138,21 @@ CREATE OR REPLACE FUNCTION `$PROJECT_ID.$DATASET.get_isodistance_concave_hull`(l
 ));
 
 -- wrapper for GEOGRAPHY to GEOJSON
-CREATE OR REPLACE FUNCTION `$PROJECT_ID.$DATASET.get_isodistance_convex_hull`(lines array<GEOGRAPHY>, start GEOGRAPHY, max_cost FLOAT64) AS ((
-  WITH SOME_NETWORK AS (
-    SELECT concat('{"type": "FeatureCollection", "features": [{"type": "Feature","geometry":', string_agg(ST_ASGEOJSON(line), '},{"type":"Feature","geometry":'), "}]}") geojson,
-    `$PROJECT_ID.$DATASET.find_nearest_point`(start, array_agg(line)) start_nearest,
-    FROM unnest(lines) line
-  ),
-  OUTPUT AS (
-    SELECT `$PROJECT_ID.$DATASET.get_isodistance_convex_hull_from_geojson`(geojson, ST_X(start_nearest), ST_Y(start_nearest), max_cost) myresult
-    FROM SOME_NETWORK
-  )
-
-  SELECT * FROM OUTPUT
-));
-
--- old convex hull using SQL
 -- CREATE OR REPLACE FUNCTION `$PROJECT_ID.$DATASET.get_isodistance_convex_hull`(lines array<GEOGRAPHY>, start GEOGRAPHY, max_cost FLOAT64) AS ((
---   SELECT ST_CONVEXHULL(ST_GEOGFROMGEOJSON(`$PROJECT_ID.$DATASET.find_points_around`(lines, start, max_cost)))
+--   WITH SOME_NETWORK AS (
+--     SELECT concat('{"type": "FeatureCollection", "features": [{"type": "Feature","geometry":', string_agg(ST_ASGEOJSON(line), '},{"type":"Feature","geometry":'), "}]}") geojson,
+--     `$PROJECT_ID.$DATASET.find_nearest_point`(start, array_agg(line)) start_nearest,
+--     FROM unnest(lines) line
+--   ),
+--   OUTPUT AS (
+--     SELECT `$PROJECT_ID.$DATASET.get_isodistance_convex_hull_from_geojson`(geojson, ST_X(start_nearest), ST_Y(start_nearest), max_cost) myresult
+--     FROM SOME_NETWORK
+--   )
+
+--   SELECT * FROM OUTPUT
 -- ));
+
+CREATE OR REPLACE FUNCTION `$PROJECT_ID.$DATASET.get_isodistance_convex_hull`(lines array<GEOGRAPHY>, start GEOGRAPHY, max_cost FLOAT64) AS ((
+  SELECT ST_CONVEXHULL(ST_GEOGFROMGEOJSON(`$PROJECT_ID.$DATASET.find_points_around`(lines, start, max_cost)))
+));
 
